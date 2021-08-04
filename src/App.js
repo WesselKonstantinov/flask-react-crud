e065a5d2409d80ef71d835a326c2bc5203d43731
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-08-04 15:20:28 +0200

    Create main section (button to add books and book table)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import { createTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
+import { useState } from 'react';
+import { createTheme, makeStyles, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 import Header from './components/Header';
+import BookTable from './components/BookTable';
+import amber from '@material-ui/core/colors/amber';
+import red from '@material-ui/core/colors/red';
 
-let theme = createTheme();
+// Override theme colors to be used for buttons
+let theme = createTheme({
+  palette: {
+    error: {
+      light: red[200],
+      main: red[500]
+    },
+    warning: {
+      light: amber[200],
+      main: amber[500]
+    }
+  }
+});
 theme = responsiveFontSizes(theme);
 
+const useStyles = makeStyles(theme => ({
+  buttonGreen: {
+    backgroundColor: theme.palette.success.main,
+    color: '#fff',
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    width: '33%',
+    '&:hover': {
+      backgroundColor: theme.palette.success.light
+    }
+  }
+}));
+
+// Mock data
+const allBooks = [
+  { id: 1, title: 'boek 1', author: 'auteur 1', read: false },
+  { id: 2, title: 'boek 2', author: 'auteur 2', read: true },
+  { id: 3, title: 'boek 3', author: 'auteur 3', read: false },
+  { id: 4, title: 'boek 4', author: 'auteur 4', read: false },
+  { id: 5, title: 'boek 5', author: 'auteur 5', read: true }
+]
+
 function App() {
+  const classes = useStyles();
+
+  const [books, setBooks] = useState(allBooks);
   return (
-    <div className="App">
+    <>
       <ThemeProvider theme={theme}>
         <Container>
           <Header />
           <Divider />
+          <main>
+            <Button className={classes.buttonGreen}>Add book</Button>
+            <BookTable books={books} />
+          </main>
         </Container>
       </ThemeProvider>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/BookTable.js
@@ -0,0 +1,65 @@
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    buttonYellow: {
+        backgroundColor: theme.palette.warning.main,
+        color: theme.palette.warning.contrastText,
+        '&:hover': {
+            backgroundColor: theme.palette.warning.light
+        }
+    },
+    buttonRed: {
+        backgroundColor: theme.palette.error.main,
+        color: theme.palette.error.contrastText,
+        '&:hover': {
+            backgroundColor: theme.palette.error.light
+        }
+    }
+}))
+
+function BookTable({ books }) {
+    const classes = useStyles();
+
+    const bookRows = books.map(book =>
+        <TableRow key={book.id}>
+            <TableCell>{book.title}</TableCell>
+            <TableCell>{book.author}</TableCell>
+            <TableCell>{book.read ? 'Yes' : 'No'}</TableCell>
+            <TableCell>
+                <ButtonGroup variant="contained" fullWidth>
+                    <Button className={classes.buttonYellow}>Update</Button>
+                    <Button className={classes.buttonRed}>Delete</Button>
+                </ButtonGroup>
+            </TableCell>
+        </TableRow>
+    )
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Author</TableCell>
+                        <TableCell>Read?</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {bookRows}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+export default BookTable;
