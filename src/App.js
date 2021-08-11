31758684ef68095864b2b1cb5b8e288c9c3754d7
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-08-11 14:17:54 +0200

    Replace mock data with data from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { createTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
@@ -23,54 +23,49 @@ let theme = createTheme({
 });
 theme = responsiveFontSizes(theme);
 
-// Mock data
-const allBooks = [
-  { id: 1, title: 'boek 1', author: 'auteur 1', read: false },
-  { id: 2, title: 'boek 2', author: 'auteur 2', read: true },
-  { id: 3, title: 'boek 3', author: 'auteur 3', read: false },
-  { id: 4, title: 'boek 4', author: 'auteur 4', read: false },
-  { id: 5, title: 'boek 5', author: 'auteur 5', read: true }
-]
+const queryClient = new QueryClient();
 
 function App() {
-  const [books, setBooks] = useState(allBooks);
+  // const [books, setBooks] = useState(allBooks);
 
-  const addBook = (title, author, read) => {
-    const newBook = { id: new Date().valueOf(), title, author, read };
-    setBooks([...books, newBook]);
-  };
+  // const addBook = (title, author, read) => {
+  //   const newBook = { id: new Date().valueOf(), title, author, read };
+  //   setBooks([...books, newBook]);
+  // };
 
-  const deleteBook = (id) => {
-    const remainingBooks = books.filter(book => id !== book.id);
-    setBooks(remainingBooks);
-  };
+  // const deleteBook = (id) => {
+  //   const remainingBooks = books.filter(book => id !== book.id);
+  //   setBooks(remainingBooks);
+  // };
 
-  const updateBook = (id, title, author, read) => {
-    const updatedBooks = books.map(book => {
-      if (id === book.id) {
-        return { ...book, title, author, read };
-      }
-      return book;
-    });
-    setBooks(updatedBooks);
-  };
+  // const updateBook = (id, title, author, read) => {
+  //   const updatedBooks = books.map(book => {
+  //     if (id === book.id) {
+  //       return { ...book, title, author, read };
+  //     }
+  //     return book;
+  //   });
+  //   setBooks(updatedBooks);
+  // };
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Container>
-          <Header />
-          <Divider />
-          <main>
-            <AddBookDialog addBook={addBook} />
-            <BookTable
-              books={books}
-              deleteBook={deleteBook}
-              updateBook={updateBook}
-            />
-          </main>
-        </Container>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Container>
+            <Header />
+            <Divider />
+            <main>
+              <AddBookDialog /*addBook={addBook}*/ />
+              <BookTable
+              // books={books}
+              // deleteBook={deleteBook}
+              // updateBook={updateBook}
+              />
+            </main>
+          </Container>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
--- src/components/BookTable.js
+++ src/components/BookTable.js
@@ -1,3 +1,4 @@
+import { useQuery } from 'react-query';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -5,42 +6,53 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import UpdateBookDialog from './UpdateBookDialog';
 import DeleteBookDialog from './DeleteBookDialog';
 
-function BookTable({ books, deleteBook, updateBook }) {
-    const bookRows = books.map(book =>
-        <TableRow key={book.id}>
-            <TableCell>{book.title}</TableCell>
-            <TableCell>{book.author}</TableCell>
-            <TableCell>{book.read ? 'Yes' : 'No'}</TableCell>
-            <TableCell>
-                <ButtonGroup variant="contained" fullWidth>
+const getBooks = async () => {
+    const response = await fetch('http://localhost:5000/books');
+    return response.json();
+}
+
+function BookTable({ deleteBook, updateBook }) {
+    const { isLoading, error, data } = useQuery('books', getBooks);
+
+    if (isLoading) {
+        return <Typography>Loading books...</Typography>
+    } else if (error) {
+        return <Typography>An error has occurred: {error.message}</Typography>
+    } else {
+        return (
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Title</TableCell>
+                            <TableCell>Author</TableCell>
+                            <TableCell>Read?</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map(book =>
+                            <TableRow key={book.id}>
+                                <TableCell>{book.title}</TableCell>
+                                <TableCell>{book.author}</TableCell>
+                                <TableCell>{book.read ? 'Yes' : 'No'}</TableCell>
+                                <TableCell>
+                                    {/* <ButtonGroup variant="contained" fullWidth>
                     <UpdateBookDialog updateBook={updateBook} book={book} />
                     <DeleteBookDialog deleteBook={deleteBook} id={book.id} />
-                </ButtonGroup>
-            </TableCell>
-        </TableRow>
-    );
-
-    return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Title</TableCell>
-                        <TableCell>Author</TableCell>
-                        <TableCell>Read?</TableCell>
-                        <TableCell></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {bookRows}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+                </ButtonGroup> */}
+                                </TableCell>
+                            </TableRow>)}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        );
+    }
 }
 
 export default BookTable;
